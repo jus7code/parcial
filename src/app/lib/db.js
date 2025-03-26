fbef0455be6f4342454fd540dca068cbe2fd726a
rodrigo <rodrigoafz26@gmail.com>
2025-03-25 22:39:11 -0500

    Prueba 1

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -6,4 +6,4 @@ const globalForPrisma = globalThis; // Uso de globalThis en lugar de global
 export const prisma =
   globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
